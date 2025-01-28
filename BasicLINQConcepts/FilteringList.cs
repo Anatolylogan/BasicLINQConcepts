62567c3d82bb42f3628ff71950effebd0558faa0
Анатолий <homenkotolya2@gmail.com>
2025-01-28 18:48:17 +0700

    Добавьте файлы проекта.

---
+++ BasicLINQConcepts/FilteringList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicLINQConcepts
+{
+    class FilteringList
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 5, 15, 20, 3, 10, 8, 25 };
+
+            List<int> filteredNumbers = numbers.Where(n => n > 10).ToList();
+
+            string result = string.Join(",", filteredNumbers);
+
+            Console.WriteLine("Числа больше 10:");
+            Console.WriteLine(result);
+        }
+    }
+}
