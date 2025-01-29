fd6f728464bfb26e541fef58e6020b685741add4
Анатолий <homenkotolya2@gmail.com>
2025-01-29 22:05:08 +0700

    2. Возвел числа  в квадрат с помощью метода LINQ , Метод Select(n =>n *n) - Применяет операцию возведение в квадрат к кажому числу.

--- BasicLINQConcepts/Program.cs
+++ BasicLINQConcepts/Program.cs
@@ -9,20 +9,22 @@ namespace BasicLINQConcepts
         static void Main()
         {
             List<int> numbers = new List<int> { 1, 5, 15, 20, 3, 10, 8, 25 };
-
             var filteredNumbers = numbers.Where(n => n > 10).ToList();
-
             string result = string.Join(",", filteredNumbers);
             Console.WriteLine("Числа больше 10:");
             Console.WriteLine(result);
 
             List<string> words = new List<string> { "яблоко", "банан", "груша", "ананас", "вишня" };
-
             var sortedWords = words.OrderBy(words => words).ToList();
-
             string result2 = string.Join(",", sortedWords);
             Console.WriteLine("Отсортированные строки: ");
             Console.WriteLine(result2);
+
+            List<int> numbers2 = new List<int> { 2, 3, 4, 5 };
+            List<int> squaredNumbers = numbers2.Select(n => n * n).ToList();
+            string result3 = string.Join(",", squaredNumbers);
+            Console.WriteLine("Квадратные числа: ");
+            Console.WriteLine(result3);
         }
     }
 }
